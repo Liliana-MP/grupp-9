0962e7c694b4b3c06280db42711418a8e00fb5ad
Liliana Montini Pitra <liliana.pitra@gmail.com>
2021-03-30 10:44:59 +0200

    added one more product to json file and added button and more styling to products

--- all-prodcuts.js
+++ all-prodcuts.js
@@ -4,7 +4,7 @@ fetch("db.json").then(function(response) {
     response.json().then(function(data){
         data.product.forEach(element => {
             console.log(element);
-            document.getElementById("row").innerHTML += ` <div class="col-lg-4 col-md-6 col-sm-12">
+            document.getElementById("row").innerHTML += ` <div class="col-lg-3 col-md-6 col-sm-12">
             <div class="card border-dark mb-3" style="max-width: 20rem">
             <div class="card-header">${element.name}</div>
             <div class="card-body">
@@ -15,6 +15,7 @@ fetch("db.json").then(function(response) {
               <p class="product-price">
                 ${element.price}
               </p>
+              <button type="button" class="btn btn-outline-success">Köp</button>
             </div>
             </div>
           </div>`
