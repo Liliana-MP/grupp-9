2938d7bb201c4b94ee609a05afab4e31455e4403
Liliana Montini Pitra <liliana.pitra@gmail.com>
2021-03-30 10:37:06 +0200

    added js file and added products from json file to home page

---
+++ all-prodcuts.js
@@ -0,0 +1,25 @@
+fetch("db.json").then(function(response) {
+   
+
+    response.json().then(function(data){
+        data.product.forEach(element => {
+            console.log(element);
+            document.getElementById("row").innerHTML += ` <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="card border-dark mb-3" style="max-width: 20rem">
+            <div class="card-header">${element.name}</div>
+            <div class="card-body">
+                <img class="product-img" src=${element.image} alt="bild på ${element.name}">
+              <p class="card-text">
+                ${element.description}
+              </p>
+              <p class="product-price">
+                ${element.price}
+              </p>
+            </div>
+            </div>
+          </div>`
+        })
+    })
+})
+
+
