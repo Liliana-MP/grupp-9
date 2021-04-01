86484bbe6be0c2d1d182a2c93dadf450058fab31
simonnojd <simon.nojd.jobb@gmail.com>
2021-04-01 14:17:13 +0200

    Betalning

--- scripts/checkout-script.js
+++ scripts/checkout-script.js
@@ -1,3 +1,4 @@
+
 var product = JSON.parse(localStorage.getItem("productsInCart")) || []
 
 getItemFromLocalStorage();
@@ -6,9 +7,6 @@ function getItemFromLocalStorage() {
   product.forEach(product => {
     createDataInCart(product.name, product.price, product.quantityInCart, product.id)
   }); 
-  $(".remove-btn").click(removeProduct)
-  $(".decrease-btn").click(decreaseQuantity)
-  $(".increase-btn").click(increaseQuantity)
   updateTotal();
 }
 
@@ -26,6 +24,7 @@ function createDataInCart(name, price, quantity, id) {
     
   </tr>
 </tbody>`
+updateTotal();
 }
 
 function updateTotal() {
@@ -36,34 +35,27 @@ function updateTotal() {
   $("#total-amount").text(total.toFixed(2) + " SEK")
 }
 
-function removeProduct() {
-    $(this).parent().parent().parent().remove();
-    product = product.filter(item => item.id != this.id)
-    saveCart()
-    updateTotal()
-}
 
-function decreaseQuantity() {
-  var temp = product.find(item => item.id == this.id)
-  if (temp.quantityInCart > 1) {
-    temp.quantityInCart--
-    $(this).parent().children("span").text(temp.quantityInCart)
-    updateTotal()
-    saveCart()
-  }
-  else {
-    temp.quantityInCart = 1
-  }
-}
-
-function increaseQuantity() {
-  var temp = product.find(item => item.id == this.id)
-  temp.quantityInCart++
-  $(this).parent().children("span").text(temp.quantityInCart)
-  updateTotal()
-  saveCart()
-}
-
-function saveCart() {
-  localStorage.setItem("productsInCart", JSON.stringify(product))
-}
+$(document).ready(function() {
+  fetchUser();
+  function fetchUser() {
+      let $loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
+      let $firstname = JSON.parse(localStorage.getItem("firstname"));
+      console.log($firstname);
+  
+      let $fName = $loggedInUser.firstname;
+      let $lName = $loggedInUser.lastname;
+      let $adress = $loggedInUser.adress;
+      let $cityId = $loggedInUser.cityid;
+      console.log($cityId);
+  
+         document.forms["orderForm"]["fName"].value = $fName;
+         document.forms["orderForm"]["lName"].value = $lName;
+         document.forms["orderForm"]["adrs"].value = $adress;
+         document.forms["orderForm"]["city"].value = $cityId;
+  
+  
+       }
+  
+       
+      });
