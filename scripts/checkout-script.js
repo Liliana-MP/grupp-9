fe9a8340a1adf0992f7ba695209525e3930ab871
simonnojd <simon.nojd.jobb@gmail.com>
2021-04-01 15:04:58 +0200

    Checkout fix

--- scripts/checkout-script.js
+++ scripts/checkout-script.js
@@ -42,15 +42,52 @@ $(document).ready(function() {
       let $lName = $loggedInUser.lastname;
       let $adress = $loggedInUser.adress;
       let $zipcode = $loggedInUser.zipcode;
-      let $cityId = $loggedInUser.cityid;
+      // City ska fixas när vi har databas
+      // let $cityId = $loggedInUser.cityid;
 
       document.forms["confirm-form"]["fName"].value = $fName;
       document.forms["confirm-form"]["lName"].value = $lName;
       document.forms["confirm-form"]["adrs"].value = $adress;
       document.forms["confirm-form"]["zip"].value = $zipcode;
-      document.forms["confirm-form"]["city"].value = $cityId;
+      // City ska fixas när vi har databas
+      // document.forms["confirm-form"]["city"].value = $cityId;
   
   
   
        }  
   });
+
+  let resource = "./db.json"
+  const $alert = $("#alert");
+
+async function checkForm(form) {
+    
+    let $firstname = $("#fName").val();
+    let $lastname = $("#lName").val();
+    let $adress = $("#adrs").val();
+    let $zipCode = $("#zip").val();
+
+    if (
+      $firstname == "" ||
+      $lastname == "" ||
+      $adress == "" ||
+      $zipCode == "" 
+      ) {
+      $alert.text("* Fyll i alla fälten");
+    }
+    else if(isNaN($zipCode)){
+      $alert.text("* Enbart siffror i postnummer fältet")
+    } 
+    else if(!isNaN($firstname)){
+      $alert.text("* Enbart bokstäver i förnamn fältet")
+    } 
+    else if(!isNaN($lastname)){
+      $alert.text("* Enbart bokstäver i efternamn fältet")
+    } 
+    else if($adress.length <= 2){
+      $alert.text("* Adressen måste ha mer än 2 bokstäver")
+    } 
+    else {
+      form.submit();
+    }
+  }
