b7e3f292d9b59487a42590ac32bbda3bed987431
robinmartinsson <martinsson.r@gmail.com>
2021-04-01 10:32:05 +0200

    saved_loggedIn_user_localStorage

--- scripts/customer-index.js
+++ scripts/customer-index.js
@@ -1,37 +1,53 @@
+$(document).ready(function() {
+fetchUser();
 function fetchUser() {
     let $loggedInUser = JSON.parse(localStorage.getItem("loggedInUser"));
     let $firstname = JSON.parse(localStorage.getItem("firstname"));
     // console.log($firstname);
-
     // let $user = $("id för formulär");
+    let $uName = $loggedInUser.firstname + " " + lastname;
+      let $adress = $loggedInUser.adress;
+      let $email = $loggedInUser.email;
+      let $tNum = $loggedInUser.phonenumber;
+      let $cityId = $loggedInUser.cityid;
 
-    for (let i = 0; i < $loggedInUser.length; i++) {
-      let $uName = $loggedInUser[i].firstname + lastname;
-      let $adress = $loggedInUser[i].adress;
-      let $email = $loggedInUser[i].email;
-      let $tNum = $loggedInUser[i].;
 
-      $($user).append(
-        "<li class='item-name'>" +
-          "<div class='order-img'>" +
-          "<img class='item-img' src=" +
-          $name +
-          ">" +
-          "</div>" +
-          "<div class='order-text'>" +
-          "<span class='item-qty'>" +
-          $p +
-          " st</span>" +
-          " " +
-          "<span>" +
-          $ +
-          "</span>" +
-          " " +
-          "<span class='item-price'>" +
-          $ +
-          " kr" +
-          "</span>" +
-          "</div>" +
-          "</li>"
-      );
-    }
+
+       document.forms["orderForm"]["fName"].value = $uName;
+    //    document.forms["orderForm"]["adress"].value = $adress;
+    //    document.forms["orderForm"]["email"].value = $email;
+    //    document.forms["orderForm"]["phonenumber"].value = $tNum;
+    //    document.forms["orderForm"]["cityid"].value = $cityId;
+
+    // for (let i = 0; i < $loggedInUser.length; i++) {
+    //   let $uName = $loggedInUser[i].firstname + lastname;
+    //   let $adress = $loggedInUser[i].adress;
+    //   let $email = $loggedInUser[i].email;
+    //   let $tNum = $loggedInUser[i].phonenumber;
+    //   let $cityId = $loggedInUser[i].cityid;
+
+    // //   $($user).append(
+    // //     "<li class='item-name'>" +
+    // //       "<div class='order-img'>" +
+    // //       "<img class='item-img' src=" +
+    // //       $name +
+    // //       ">" +
+    // //       "</div>" +
+    // //       "<div class='order-text'>" +
+    // //       "<span class='item-qty'>" +
+    // //       $p +
+    // //       " st</span>" +
+    // //       " " +
+    // //       "<span>" +
+    // //       $ +
+    // //       "</span>" +
+    // //       " " +
+    // //       "<span class='item-price'>" +
+    // //       $ +
+    // //       " kr" +
+    // //       "</span>" +
+    // //       "</div>" +
+    // //       "</li>"
+    // //   );
+     }
+    });
