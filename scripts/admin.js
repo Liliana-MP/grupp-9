51fa208789aa8213ca54b499a53a96a7d657bc55
yadonx <ej_yo@hotmail.com>
2021-04-06 16:33:06 +0200

    added render_product

--- scripts/admin.js
+++ scripts/admin.js
@@ -8,7 +8,7 @@ async function validate_product(form) {
     let $quantity = $("#product-quantity").val()
     let $price = $("#product-price").val()
 
-    const errorTxt = $("#alert")
+    const errorTxt = $("#alert-two")
 
 
     var responce = await fetch(resource)
@@ -42,18 +42,39 @@ async function validate_product(form) {
     
   }
 
+ 
 
-  function validateNotEmpty() {
-    let $name = $("#product-name").val()
-    let $producer = $("#product-producer").val()
-    let $category = $("#product-category").val()
-    let $quantity = $("#product-quantity").val()
-    let $price = $("#product-price").val()
 
-    if ($name == "" || $producer == ""|| $category == ""|| $quantity == ""|| $price == "")
-      return false;
-    else
-      return true;
-  
+  async function render_product(){
+    let $product = $("#search-product").val()
+    let $name = $("#product-name")
+    let $producer = $("#product-producer")
+    let $category = $("#product-category")
+    let $quantity = $("#product-quantity")
+    let $price = $("#product-price")
+
+    const errorTxt = $("#alert-one")
+
+    var responce = await fetch(resource)
+    var data = await responce.json()
+
+    const product = data.product.find(item => item.name.toUpperCase() == $product.toUpperCase())
+    const producer = await data.company.find(item => item.id == product.companyid)
+    const category = await data.category.find(item => item.id == product.categoryid)
+    
+    if(product !== undefined){
+        $name.val(product.name)
+        $producer.val(producer.name)
+        $category.val(category.name)
+        $quantity.val(product.quantity)
+        $price.val(product.price)
+    }
+    else errorTxt.text("* Produkten finns inte")
+
+  }
+
+
+  function remove_product(){
+
   }
   
